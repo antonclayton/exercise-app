5934d2cf54e67d9ee3ced40023e2a5ef683b2ab2
Anton Clayton <anton.clayton@gmail.com>
2024-01-28 17:16:07 -0800

    Home page banner completed

---
+++ src/Components/SearchExercises.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchExercises = () => {
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default SearchExercises
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import Banner from '../Components/Banner'
+import Exercises from '../Components/Exercises'
+import SearchExercises from '../Components/SearchExercises'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Box>
+      <Banner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
 
